d1cb0f610fb231b07fb6039bfbeacdddfc135119
jessegarcia2049 <159494595+jessegarcia2049@users.noreply.github.com>
2024-02-09 17:02:27 -0600

    Add files via upload

---
+++ Practica 2/JavaApplication48/src/javaapplication48/JavaApplication48.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication48;
+
+/**
+ *
+ * @author invitado
+ */
+public class JavaApplication48 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //VARIABLES:
+             //TIPO DE DATO NOMBRE DEL IDENTIFICADOR
+    //short int byte long --> enteros
+    //float double --> numeros con decimal
+    //char --> character --> caracteres
+    //boolean --> verdadero o falso
+    //String --> cadenas de textos 
+    // si no se declara una variable, no se puede usar
+    int edad; // declaración de una variable
+    String nombre;
+    double salario;
+    
+    //ASIGNACIÓN DE VALORES:
+    //IDENTIFICADOR = VALOR;
+    //se coloca el valor 50 dentro de la variable edad
+    edad = 50;
+    nombre = "juan perez jolote";
+    salario = 123.56;
+    //COMO LO USAMOS:
+    System.out.println (nombre); 
+    System.out.println (edad);
+    System.out.println(salario);
+    }
+            
+}
