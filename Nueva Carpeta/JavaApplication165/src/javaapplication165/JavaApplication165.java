1162c2756c28fada7b015715c56d278ebccfd5f5
jessegarcia2049 <159494595+jessegarcia2049@users.noreply.github.com>
2024-02-15 17:02:37 -0600

    Add files via upload
    uN POCO de reborujo aun en proceso de compra de mi laptop..
    Y a lahora de guardar como pudo haber un incidente que letras se ponen en negro.

---
+++ Nueva Carpeta/JavaApplication165/src/javaapplication165/JavaApplication165.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication165;
+
+/**
+ *
+ * @author invitado
+ */
+public class JavaApplication165 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        string nombre;//DECLARACIÓN
+        nombre = Jesus Aristoteles Quinones Garcia; ASIGNACIÓN --> UN SOLO = SIGNIFICA ASIGNAR
+                            int edad;// DECLARACIÓN
+                    System.out.println ("Tu nombre es") ; 
+                    System.out.println (nombre)
+    edad = 42;  //ASIGNACIÓN --> UN SOLO = SIGNIFICA ASIGNAR
+    System.out.println ("Tu edad es") ;
+   
+        System.out.println (edad) ;
+    }
+    
+}
---
+++ Nueva Carpeta/JavaApplication166/src/javaapplication166/JavaApplication166.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication166;
+
+/**
+ *
+ * @author invitado
+ */
+public class JavaApplication166 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic hereString nombre;//DECLARACIÃ“N
+        nombre = "Jesus Aristoteles Quinones Garcia"; // ASIGNACION --> UN SOLO = SIGNIFICA ASIGNAR
+                                       System.out.println ("Tu nombre es") ; 
+                    System.out.println ("nombre");
+                         int edad;// DECLARACIÃ“N
+    edad = 42;  //ASIGNACIÃ“N --> UN SOLO = SIGNIFICA ASIGNAR
+            System.out.println ("Tu edad es") ;
+           System.out.println (edad) ;
+    }
+    
+}
